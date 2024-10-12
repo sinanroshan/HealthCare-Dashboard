45550231fed8e0cda60bc951f10faa78928830b4
Sinan Roshan <sinan@aufaitux.com>
2024-10-12 15:23:21 +0530

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopNavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/top-nav/top-nav.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-nav',
+  templateUrl: './top-nav.component.html',
+})
+export class TopNavComponent {
+
+}
